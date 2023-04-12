7e21d4fa07150ff40bbc13167f6cb1ec6d9dbd05
Khemis Louison <khemis@air-de-khemis.home>
2023-04-12 10:35:34 +0200

    add-front-end

--- src/components/addWilder/AddWilder.tsx
+++ src/components/addWilder/AddWilder.tsx
@@ -1,37 +1,50 @@
-import React, { FormEvent, useContext, useState } from "react";
+import React, { useContext } from "react";
 import axios from "axios";
 import "./AddWilder.css";
 import { WildersContext } from "../../contexts/WilderContext";
+import { useForm, SubmitHandler } from "react-hook-form";
+// import { FormEvent } from "react";
 //////////// interface ////////////
 
+interface FormValues {
+  name: string;
+  city: string;
+}
 //////////// finish ////////////
 
 const AddWilder = () => {
   //////////// logique ////////////
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
+  // const [name, setName] = useState("");
+  // const [city, setCity] = useState("");
   const { fetchData } = useContext(WildersContext);
-  const handleSubmitAdd = async (e: FormEvent) => {
-    e.preventDefault();
-    await axios.post("http://localhost:3001/api/wilder", {
-      name,
-      city,
-    });
+
+  // const handleSubmitAdd = async (e: FormEvent) => {
+  //   e.preventDefault();
+  //   await axios.post("http://localhost:3001/api/wilder", {
+  //     name,
+  //     city,
+  //   });
+  //   fetchData();
+  // };
+  console.log();
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    // e.preventDefault();
+    await axios.post("http://localhost:3001/api/wilder", data);
     fetchData();
   };
-  console.log();
-
   //////////// finish ////////////
 
   return (
-    <form className="form-add" onSubmit={handleSubmitAdd}>
+    <form className="form-add" onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group name">
         <label>Name</label>
         <input
           className="input-add"
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          {...register("name")}
+          // value={name}
+          // onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div className="form-group city">
@@ -39,8 +52,9 @@ const AddWilder = () => {
         <input
           className="input-add"
           type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
+          {...register("city")}
+          // value={city}
+          // onChange={(e) => setCity(e.target.value)}
         />
       </div>
 
